d6ca368f9b140d8aedd246a2fc2524658ec5438c
bo0tzz <btdewilligen@gmail.com>
2016-06-07 12:42:59 +0200

    Better fix for to/that issue

--- src/main/java/me/bo0tzz/remindmebot/api/RemindMeBotListener.java
+++ src/main/java/me/bo0tzz/remindmebot/api/RemindMeBotListener.java
@@ -66,7 +66,13 @@ public class RemindMeBotListener implements Listener {
             event.getChat().sendMessage("Give me ");
             return;
         }
-        String[] args = event.getArgsString().replaceFirst(" to ", " that ").split("that", 2);
+
+        String str = event.getArgsString();
+        int idx, idx2 = -1;
+        if ((idx = str.indexOf(" to ")) != -1 && idx < (idx2 = str.indexOf(" that ")) || idx2 == -1) {
+            str = str.replaceFirst(" to ", " that ");
+        }
+        String[] args = str.split("that", 2);
 
         if (args.length != 2) {
             event.getChat().sendMessage("Something went wrong while processing your reminder! Please try again");
